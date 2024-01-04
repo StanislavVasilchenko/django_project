88c37e10bf83ed536bce50ba39c94808c571fe86
stanislav <stanislav.vasilchenko@yandex.ru>
2024-01-04 22:50:13 +0300

    Subpatterns

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from main.views import index
+from main.views import index, contact
 
 app_name = 'main'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('contact/', contact, name='contact'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 
+from main.models import Student
+
 
 def index(request):
+    students_list = Student.objects.all()
+    context = {
+        'object_list': students_list,
+        'title': 'Главная',
+    }
+    return render(request, 'main/index.html', context)
+
+
+def contact(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
         message = request.POST.get('message')
         print(f'Name: {name}, Email: {email}, Message: {message}')
-    return render(request, 'main/index.html')
+    context = {
+        'title': 'Контакты',
+    }
+    return render(request, 'main/contact.html', context)
